103c8013eff55c3b9fe01c00f700a9ad4eedc83b
Duncan Bell <61971007+duncanmadetech@users.noreply.github.com>
2020-04-23 13:59:26 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,12 +10,12 @@ variable "db_table_name" {
 
 variable "db_read_capacity" {
   type    = number
-  default = 1
+  default = 2
 }
 
 variable "db_write_capacity" {
   type    = number
-  default = 1
+  default = 2
 }
 
 variable "tag_user_name" {
